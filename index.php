e29fa80caee145c78e3704ba1c96dffcf72f7b7a
César Dutra <cmdbsb@gmail.com>
2018-09-25 21:49:10 -0300

    Criamos a classe usuário

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ require_once "config.php";
 
 $sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+//echo json_encode($usuarios);
+
+$root = new Usuario();
+
+$root->loadById(3);
+
+echo $root;
 
  ?>
